d1ea80117f178c1d21d1942da18d5a1900834f45
lrnzsp <112730067+lrnzsp@users.noreply.github.com>
2024-11-27 09:54:06 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    darkMode: 'class', // Abilita il tema scuro
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}", // Assicura che Tailwind scansioni tutti i file
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'], // Font moderno
+            },
+            colors: {
+                background: '#000',
+                foreground: '#fff',
+                primary: '#1e90ff',
+                secondary: '#00bfff',
+            },
+        },
+    },
+    plugins: [],
+};
